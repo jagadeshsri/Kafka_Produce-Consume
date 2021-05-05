267fd1bc0b5dd98e88eb98b5e63abe2a5d366227
jagadeshsri <55649191+jagadeshsri@users.noreply.github.com>
2021-05-05 16:11:51 +0530

    Add files via upload

--- kakfa-producer-consumer-example/src/main/java/com/gaurav/kafka/producer/ProducerCreator.java
+++ kakfa-producer-consumer-example/src/main/java/com/gaurav/kafka/producer/ProducerCreator.java
@@ -1,23 +1,24 @@
 package com.gaurav.kafka.producer;
 
-import java.util.Properties;
-
+import com.fasterxml.jackson.databind.JsonNode;
+import com.gaurav.kafka.constants.IKafkaConstants;
+import com.gaurav.kafka.pojo.Data;
+import com.gaurav.kafka.serializer.CustomSerializer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.LongSerializer;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
 
-import com.gaurav.kafka.constants.IKafkaConstants;
+import java.util.Properties;
 
 public class ProducerCreator {
 
-	public static Producer<String, String> createProducer() {
+	public static Producer<Integer, Data> createProducer() {
 		Properties props = new Properties();
 		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, IKafkaConstants.KAFKA_BROKERS);
 		props.put(ProducerConfig.CLIENT_ID_CONFIG, IKafkaConstants.CLIENT_ID);
-		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class.getName());
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, CustomSerializer.class.getName());
 		return new KafkaProducer<>(props);
 	}
 }
