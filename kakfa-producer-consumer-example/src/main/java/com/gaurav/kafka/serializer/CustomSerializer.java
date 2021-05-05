dd031770c8e6a3b05888a7cecb24d417dce56698
jagadeshsri <55649191+jagadeshsri@users.noreply.github.com>
2021-05-05 16:12:16 +0530

    Add files via upload

--- kakfa-producer-consumer-example/src/main/java/com/gaurav/kafka/serializer/CustomSerializer.java
+++ kakfa-producer-consumer-example/src/main/java/com/gaurav/kafka/serializer/CustomSerializer.java
@@ -2,12 +2,15 @@ package com.gaurav.kafka.serializer;
 
 import java.util.Map;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.gaurav.kafka.pojo.Data;
+import org.apache.kafka.common.errors.SerializationException;
 import org.apache.kafka.common.serialization.Serializer;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.gaurav.kafka.pojo.CustomObject;
 
-public class CustomSerializer implements Serializer<CustomObject> {
+public class CustomSerializer implements Serializer<Data> {
 
 	@Override
 	public void configure(Map<String, ?> configs, boolean isKey) {
@@ -15,15 +18,16 @@ public class CustomSerializer implements Serializer<CustomObject> {
 	}
 
 	@Override
-	public byte[] serialize(String topic, CustomObject data) {
-		byte[] retVal = null;
-		ObjectMapper objectMapper = new ObjectMapper();
-		try {
-			retVal = objectMapper.writeValueAsString(data).getBytes();
-		} catch (Exception exception) {
-			System.out.println("Error in serializing object" + data);
+	public byte[] serialize(String s, Data data) {
+		ObjectMapper mapper = new ObjectMapper();
+		if (data == null) {
+			return null;
 		}
-		return retVal;
+		try {
+			return mapper.writeValueAsBytes(data);
+		} catch (JsonProcessingException e) {
+			throw new SerializationException("Error serializing JSON message", e);
+		}
 	}
 
 	@Override
