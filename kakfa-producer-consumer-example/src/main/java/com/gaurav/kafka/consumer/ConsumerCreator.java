dfce1f389d10c944e379c5fb7c69aaf553269ebe
jagadeshsri <55649191+jagadeshsri@users.noreply.github.com>
2021-05-05 16:10:13 +0530

    Add files via upload

--- kakfa-producer-consumer-example/src/main/java/com/gaurav/kafka/consumer/ConsumerCreator.java
+++ kakfa-producer-consumer-example/src/main/java/com/gaurav/kafka/consumer/ConsumerCreator.java
@@ -6,6 +6,7 @@ import java.util.Properties;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.LongDeserializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
 import com.gaurav.kafka.constants.IKafkaConstants;
@@ -16,7 +17,7 @@ public class ConsumerCreator {
 		final Properties props = new Properties();
 		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, IKafkaConstants.KAFKA_BROKERS);
 		props.put(ConsumerConfig.GROUP_ID_CONFIG, IKafkaConstants.GROUP_ID_CONFIG);
-		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class.getName());
 		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 		props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, IKafkaConstants.MAX_POLL_RECORDS);
 		props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
